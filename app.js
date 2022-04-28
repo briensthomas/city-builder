f485670d1b4ba9a4caf3ac17e98374bb5915f8ef
Brien Thomas <thomasbrien94@gmail.com>
2022-04-28 11:12:11 -0700

    created event listener(change) for each select element, correctly logs value to console.

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 // import functions and grab DOM elements
 const architectureEl = document.getElementById('architectureImage');
-const architectureSelect = document.getElementById('architecture-select');
 
+
+const architectureSelect = document.getElementById('architecture-select');
+const climateSelect = document.getElementById('climate-select');
+const mustSeeSelect = document.getElementById('mustSee-select')
 // let state
 
 // set event listeners 
 architectureSelect.addEventListener('change', () => {
     console.log(`changing Architecture to ${architectureSelect.value}`)
     architectureEl.style.backgroundImage = `url(./assets/${architectureSelect.value}-architecture.jpg')`;
+});
+
+climateSelect.addEventListener('change', () => {
+    console.log(`changing climate to ${climateSelect.value}`);
+});
+
+mustSeeSelect.addEventListener('change', () => {
+    console.log(`changing mustSee to ${mustSeeSelect.value}`);
 });
   // get user input
   // use user input to update state 
