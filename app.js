7e1f1531ee0f8c1734d64417e2a7718092d3187c
Brien Thomas <thomasbrien94@gmail.com>
2022-04-28 14:26:06 -0700

    slogan input and button correctly pass new slogans to the array, and the display slogan for let of loop correctly displays the array to the user.

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 // import functions and grab DOM elements
 const architectureEl = document.getElementById('architectureImage');
+const climateEl = document.getElementById('climateImage');
+const mustSeeEl = document.getElementById('mustSeeImage');
+const counts = document.getElementById('counts');
 
+const sloganInput = document.getElementById('sloganInput');
+const sloganButton = document.getElementById('sloganButton');
+const sloganEl = document.getElementById('slogan');
 
 const architectureSelect = document.getElementById('architecture-select');
 const climateSelect = document.getElementById('climate-select');
-const mustSeeSelect = document.getElementById('mustSee-select')
+const mustSeeSelect = document.getElementById('mustSee-select');
 // let state
+let architectCount = 0;
+let climateCount = 0;
+let mustSeeCount = 0;
 
+let sloganArray = [];
 // set event listeners 
 architectureSelect.addEventListener('change', () => {
-    console.log(`changing Architecture to ${architectureSelect.value}`)
-    architectureEl.style.backgroundImage = `url(./assets/${architectureSelect.value}-architecture.jpg')`;
+    architectureEl.style.backgroundImage = `url('./assets/${architectureSelect.value}.png')`;
+    architectCount++;
+    displayCounts();
 });
 
 climateSelect.addEventListener('change', () => {
-    console.log(`changing climate to ${climateSelect.value}`);
+    climateEl.style.backgroundImage = `url('./assets/${climateSelect.value}.png')`;
+    climateCount++;
+    displayCounts();
 });
 
 mustSeeSelect.addEventListener('change', () => {
-    console.log(`changing mustSee to ${mustSeeSelect.value}`);
+    mustSeeEl.style.backgroundImage = `url('./assets/${mustSeeSelect.value}.png')`;
+    mustSeeCount++;
+    displayCounts();
 });
+
+sloganButton.addEventListener('click', () => {
+    console.log('changing the slogan');
+    sloganArray.push(sloganInput.value);
+    sloganInput.value = ' ';
+    displaySlogan();
+});
+
+function displaySlogan() {
+    sloganEl.textContent = ' ';
+    for (let sloganInput of sloganArray) {
+        console.log(sloganArray);
+        const sloganDiv = document.createElement('div');
+        sloganEl.append(sloganDiv);
+        sloganDiv.textContent = sloganInput;
+    }
+}
+function displayCounts() {
+    counts.textContent = `You have changed the Architecture ${architectCount} times, the Climate ${climateCount} times, and the Must See Attractions ${mustSeeCount} times.`;
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
