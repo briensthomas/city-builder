3093bc9fb3a5ebe1421697de6831c67fad2ea418
Brien Thomas <thomasbrien94@gmail.com>
2022-04-28 11:06:24 -0700

    created dropdown selections for all current categories

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions and grab DOM elements
+const architectureEl = document.getElementById('architectureImage');
+const architectureSelect = document.getElementById('architecture-select');
 
 // let state
 
 // set event listeners 
+architectureSelect.addEventListener('change', () => {
+    console.log(`changing Architecture to ${architectureSelect.value}`)
+    architectureEl.style.backgroundImage = `url(./assets/${architectureSelect.value}-architecture.jpg')`;
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
