79725ccbb4cced342d3da04af54290fe578ba76b
Brien Thomas <thomasbrien94@gmail.com>
2022-04-29 14:02:53 -0700

    added City name textbox, slogan now generates the city names array in conjunction with the slogans array.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const architectureEl = document.getElementById('architectureImage');
 const climateEl = document.getElementById('climateImage');
 const mustSeeEl = document.getElementById('mustSeeImage');
 
+const cityInput = document.getElementById('cityInput');
+const cityButton = document.getElementById('cityButton');
+const cityName = document.getElementById('cityName');
+
 const sloganInput = document.getElementById('sloganInput');
 const sloganButton = document.getElementById('sloganButton');
 const sloganEl = document.getElementById('slogan');
@@ -17,28 +21,31 @@ let architectCount = 0;
 let climateCount = 0;
 let mustSeeCount = 0;
 
+let cityArray = [];
 let sloganArray = [];
 // set event listeners 
-architectureSelect.addEventListener('change', () => {
-    architectureEl.style.backgroundImage = `url('./assets/${architectureSelect.value}.png')`;
-    
-    // const img = document.createElement('img');
 
-    // img.src = `./assets/${architectureSelect.value}.png`;
-    // architectureEl.appendChild(img);
-    // architectureEl.style.backgroundImage = `url('./assets/${architectureSelect.value}.png')`;
+cityButton.addEventListener('click', () => {
+    console.log('changing city name');
+    cityName.textContent = ' ';
+    cityName.textContent = `${cityInput.value}`;
+    cityArray.push(cityInput.value);
+});
+
+architectureSelect.addEventListener('change', () => {
+    architectureEl.src = `./assets/${architectureSelect.value}.png`;
     architectCount++;
     displayCounts();
 });
 
 climateSelect.addEventListener('change', () => {
-    climateEl.style.backgroundImage = `url('./assets/${climateSelect.value}.png')`;
+    climateEl.src = `./assets/${climateSelect.value}.png`;
     climateCount++;
     displayCounts();
 });
 
 mustSeeSelect.addEventListener('change', () => {
-    mustSeeEl.style.backgroundImage = `url('./assets/${mustSeeSelect.value}.png')`;
+    mustSeeEl.src = `./assets/${mustSeeSelect.value}.png`;
     mustSeeCount++;
     displayCounts();
 });
@@ -51,27 +58,29 @@ sloganButton.addEventListener('click', () => {
 });
 
 // function to display slogans to the user
-function displaySlogan() {
+const displaySlogan = () => {
   // reset the DOM so the array we'll pass in doesn't 
   // pile back on to the list
     sloganEl.textContent = ' ';
   // for let of loop displays each slogan in the array
     for (let sloganInput of sloganArray) {
-        console.log(sloganArray);
-  // creates a new div to hold the contents of the array
-        const sloganDiv = document.createElement('div');
-  // tells the document where to add in the new element
-        sloganEl.append(sloganDiv);
-  // edits the new div element to display the text from the input box
-        sloganDiv.textContent = sloganInput;
+        for (let city of cityArray) {
+            console.log(sloganArray);
+            // creates a new div to hold the contents of the array
+            const sloganDiv = document.createElement('div');
+            // tells the document where to add in the new element
+            sloganEl.append(sloganDiv);
+            // edits the new div element to display the text from the input box
+            sloganDiv.textContent = city + ': ' + sloganInput;
+        }
     }
-}
+};
 
-function displayCounts() {
+const displayCounts = () => {
     counts.textContent = `You have changed the Architecture ${architectCount} times, 
     the Climate ${climateCount} times, and the Must See Attractions ${mustSeeCount} times, 
     and your City's Slogans are:`;
-}
+};
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
